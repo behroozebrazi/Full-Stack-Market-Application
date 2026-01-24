1bd051545e99cc3d3288733c8cff6d962adeb2be
behroozebrazi <behber@berkeley.edu>
2026-01-23 18:02:59 -0800

    add Header.js Footer.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 import './App.css'
 
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Header />
+      <Footer />
+    </div>
   )
 }
 
