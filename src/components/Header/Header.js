4a63d4230e76b7630191276da59c1b40aec6bedc
behroozebrazi <behber@berkeley.edu>
2026-01-23 18:58:16 -0800

    set up header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Container, Navbar, Nav } from 'react-bootstrap'
+
+import './Header.css'
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <Navbar className='nav-bg' variant='dark'>
+        <Container>
+          <Navbar.Brand href='/'>FC Market</Navbar.Brand>
+          <Nav>
+            <Nav.Link href='/cart'>Cart</Nav.Link>
+            <Nav.Link href='/account'>Account</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </header>
   )
 }
 
