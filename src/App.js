d17131b74e48cd77c090d18e339496b9e7937640
behroozebrazi <behber@berkeley.edu>
2026-01-24 17:03:35 -0800

    add products info and image

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import { Container } from 'react-bootstrap'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 
-import './App.css'
-
 
 const App = () => {
   return <div className='d-flex flex-column min-vh-100'>
---
+++ src/products.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Wireless headphones',
+    image: './images/product_image_1.png',
+    price: 99,
+  },
+  {
+    _id: '2',
+    name: 'Running sneakers',
+    image: './images/product_image_2.png',
+    price: 89,
+  },
+  {
+    _id: '3',
+    name: 'Fitness smartwatch',
+    image: './images/product_image_3.png',
+    price: 199,
+  },
+  {
+    _id: '4',
+    name: 'DSLR camera',
+    image: './images/product_image_4.png',
+    price: 499,
+  },
+  {
+    _id: '5',
+    name: 'Skincare collection',
+    image: './images/product_image_5.png',
+    price: 49,
+  },
+  {
+    _id: '6',
+    name: 'Leather backpack',
+    image: './images/product_image_6.png',
+    price: 79,
+  },
+  {
+    _id: '7',
+    name: 'Game controller',
+    image: './images/product_image_7.png',
+    price: 99,
+  },
+  {
+    _id: '8',
+    name: 'Perfume bottle',
+    image: './images/product_image_8.png',
+    price: 59,
+  },
+  {
+    _id: '9',
+    name: 'Watch',
+    image: './images/product_image_9.png',
+    price: 109,
+  },
+]
