c89517aed292de50d3e481e47a31fdadd647baa1
behroozebrazi <behber@berkeley.edu>
2026-01-26 14:47:28 -0800

    set up redux store, reducer, and action

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react"
 import ReactDOM from 'react-dom/client'
+import { Provider } from "react-redux"
+
+import store from "./store"
 import App from "./App"
 
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -8,4 +11,8 @@ import "./index.css"
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(<App />)
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
--- src/store.js
+++ src/store.js
@@ -1,14 +1,22 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { thunk } from 'redux-thunk'
 
+import { productListReducer } from './reducer/productReducer'
+
 
 const initialState = {}
 
 const middleWare = [thunk]
 
-const reducer = combineReducers({})
+const reducer = combineReducers({
+  productList: productListReducer
+})
 
-const store = createStore(reducer, initialState, applyMiddleware(...middleWare))
+const store = createStore(
+  reducer,
+  initialState,
+  applyMiddleware(...middleWare)
+)
 
 
 export default store
