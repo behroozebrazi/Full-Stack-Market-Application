66bb99a6fad88e4c5c536fcc2b8776bfa96c3b77
behroozebrazi <behber@berkeley.edu>
2026-01-26 18:04:42 -0800

    display product in cart plus total price

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col, ListGroup, Image, Button, Card } from 'react-bootstrap'
 
 import { addToCart } from '../action/cartAction'
 
@@ -20,8 +21,57 @@ const Cart = () => {
     }
   }, [dispatch, id])
 
+  const removeFromCartHandler = (productId) => {
+    console.log('Remove')
+  }
+
+
+  console.log(cartItems)
+
+
   return (
-    <h3>Cart</h3>
+    <Row>
+      <Col md={8}>
+        <h4>Cart</h4>
+        {cartItems.length === 0 ? <p>Cart is empty</p> :
+          <ListGroup variant='flush'>
+
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item.productId}>
+                <Row>
+
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+
+                  <Col md={3}>{item.name}</Col>
+
+                  <Col md={2}>${item.price}</Col>
+
+                  <Col md={2}>
+                    <Button type='button' variant='light' onClick={() => removeFromCartHandler(item.productId)}>
+                      <i className='fa fa-trash'></i>
+                    </Button>
+                  </Col>
+
+                </Row>
+              </ListGroup.Item>
+            ))}
+
+          </ListGroup>
+        }
+      </Col>
+
+      <Col md={4}>
+        <Card>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              Total price: ${cartItems.reduce((accumulator, currentItem) => accumulator + currentItem.price, 0)}
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </Col>
+    </Row>
   )
 }
 
